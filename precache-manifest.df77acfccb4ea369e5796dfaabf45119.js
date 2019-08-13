da8db8f2d5b00b40c1a738575b4942d6f8b3a989
Lucas Teixeira <lucas.teixeira@calindra.com.br>
2019-08-13 17:25:05 -0300

    Updates

---
+++ precache-manifest.df77acfccb4ea369e5796dfaabf45119.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "292e1e5f6b200a63bc0449f68e8d51ec",
+    "url": "/countdown-counter/index.html"
+  },
+  {
+    "revision": "2a168aee11d881fcb3b2",
+    "url": "/countdown-counter/static/css/main.3e75e0f4.chunk.css"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/countdown-counter/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "2a168aee11d881fcb3b2",
+    "url": "/countdown-counter/static/js/main.dee45598.chunk.js"
+  },
+  {
+    "revision": "6a4fbe9af7d612034ab1",
+    "url": "/countdown-counter/static/js/runtime~main.82c664b0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/countdown-counter/static/media/logo.5d5d9eef.svg"
+  }
+]);
